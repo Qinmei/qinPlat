44c9bed800b83446a86992e718d01929e6be16ec
qinvz <sun.houtao@outlook.com>
2020-03-08 21:51:42 +0800

    一些修改

--- src/settings/settings.controller.ts
+++ src/settings/settings.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { SettingsService } from './settings.service';
 
 @Controller('settings')
-export class SettingsController {}
+export class SettingsController {
+  constructor(private readonly settingsService: SettingsService) {}
+
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+}
